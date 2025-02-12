d6b1126cab7cd0febb8dfa90102096de77eccc49
DevWonder01 <55108486+DevWonder01@users.noreply.github.com>
2025-02-12 14:53:36 +0100

    add: bernoulli_equation

---
+++ src/bernoulli_equation.rs
@@ -0,0 +1,33 @@
+#[derive(Debug, Clone, Copy)]
+struct FluidPoint {
+    pressure: f64, 
+    velocity: f64,
+    height: f64, 
+}
+
+const GRAVITY: f64 = 9.81; // m/s^2
+const TOLERANCE: f64 = 1e-3; // Small tolerance for floating-point comparisons
+
+fn bernoulli_equation(fluid_density: f64, point1: FluidPoint, point2: FluidPoint) -> bool {
+    let bernoulli_constant1 = 
+        point1.pressure / fluid_density + 0.5 * point1.velocity.powi(2) + GRAVITY * point1.height;
+
+    let bernoulli_constant2 = 
+        point2.pressure / fluid_density + 0.5 * point2.velocity.powi(2) + GRAVITY * point2.height;
+
+    (bernoulli_constant1 - bernoulli_constant2).abs() < TOLERANCE
+}
+
+
+
+fn main() {
+    let fluid_density = 1000.0; 
+    let point1 = FluidPoint { pressure: 100000.0, velocity: 2.0, height: 0.0 };
+    let point2 = FluidPoint { pressure: 99800.0, velocity: 2.2, height: 0.0 };
+
+    if bernoulli_equation(fluid_density, point1, point2) {
+        println!("Bernoulli's equation is satisfied.");
+    } else {
+        println!("Bernoulli's equation is not satisfied.");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 pub mod newton_graviational_law;
 pub mod clausius_clapeyron;
 pub mod ideal_gas_law;
+pub mod bernoulli_equation;
+
 fn main() {
     println!("Physics is fun");
 }
